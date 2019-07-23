71b398beb93e73fdf259750b70e85568e4f418ab
aachen97 <51843336+aachen97@users.noreply.github.com>
2019-07-23 10:14:04 -0400

    added by aaron

--- veggie_checker.py
+++ veggie_checker.py
@@ -1,1 +1,21 @@
 #This is a veggie checker
+# need to specify location of some certificates for rasterio
+!export CURL_CA_BUNDLE=/etc/ssl/certs/ca-certificates.crt
+try:
+  import rasterio
+  import rasterio.plot
+except:
+  !pip install rasterio
+  import rasterio
+  import rasterio.plot
+  
+try:
+  import pyproj
+except:
+  !pip install pyproj
+  import pyproj
+  
+import gdal
+import numpy as np
+import matplotlib
+import matplotlib.pyplot as plt
