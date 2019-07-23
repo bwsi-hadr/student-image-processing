71b0b56660fbf8004b885fb5e67513964a9f72d8
aachen97 <51843336+aachen97@users.noreply.github.com>
2019-07-23 11:06:30 -0400

    dylan

--- analyzeimage.py
+++ analyzeimage.py
@@ -4,4 +4,5 @@ def analyzeimage(old_image):
 	# analyze the image 'old_image' here
 	# save the analyzed image to 'static/temp.jpg'
 	# return the path of the new image file
+	#dylann hella gay
         return "static/temp.jpg"
