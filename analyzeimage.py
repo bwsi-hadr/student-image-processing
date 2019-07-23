aa9ef56f5931262dd1b6c06c3660f2c84b4affdc
Dylan Pham <43.dpham@gmail.com>
2019-07-23 15:00:56 +0000

    changes

--- analyzeimage.py
+++ analyzeimage.py
@@ -4,23 +4,3 @@ def analyzeimage(old_image):
 	# analyze the image 'old_image' here
 	# save the analyzed image to 'static/temp.jpg'
 	# return the path of the new image file
-	!export CURL_CA_BUNDLE=/etc/ssl/certs/ca-certificates.crt
-	try:
-	import rasterio
-	import rasterio.plot
-	except:
-	!pip install rasterio
-	import rasterio
-	import rasterio.plot
-
-	try:
-	import pyproj
-	except:
-	!pip install pyproj
-	import pyproj
-
-	import gdal
-	import numpy as np
-	import matplotlib
-	import matplotlib.pyplot as plt
-	return "static/temp.jpg"
