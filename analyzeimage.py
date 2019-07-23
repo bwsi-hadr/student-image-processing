2b22d457751315db480fc52fc459749abdaf636d
Brian <brianxiang123@yahoo.com>
2019-07-23 11:03:59 -0400

    Added return statement

--- analyzeimage.py
+++ analyzeimage.py
@@ -4,3 +4,4 @@ def analyzeimage(old_image):
 	# analyze the image 'old_image' here
 	# save the analyzed image to 'static/temp.jpg'
 	# return the path of the new image file
+        return "static/temp.jpg"
