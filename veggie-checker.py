80dedd80ba17e68a035f6f46ffc7c37dafc0fb2e
Dylan Pham <43.dpham@gmail.com>
2019-07-25 19:54:08 +0000

    jabba

--- veggie-checker.py
+++ veggie-checker.py
@@ -1,6 +1,11 @@
 import cv2 as cv
 import numpy as np
 from matplotlib import pyplot as plt
+from PIL import Image
 
-img = cv.imread('tree.jpg')
+img = cv.imread('images/wallplants.jpg')
+ir, ir_n_g, ir_n_r = cv.split(img)
 
+ndvi = (ir + ir - ir_n_r)/(ir_n_r)
+
+cv.imshow('', ndvi)
