07e7c71e708cc54bf3e3afe14ca6eede007ddb88
Dylan Pham <32346117+dylan-pham@users.noreply.github.com>
2019-07-26 09:07:55 -0400

    Update edge_detection.py

--- edge_detection.py
+++ edge_detection.py
@@ -1,14 +1,13 @@
 import cv2 as cv
 import numpy as np
 
-# cv.startWindowThread()
-
 img = cv.imread('images/tush.jpg')
 edges = cv.Canny(img, 50, 100) # (image, lower pixel value, higher pixel value)
 
 while True:
     cv.imshow('', edges)
+    
     if cv.waitKey(0) == ord('q'):
         break
+        
 cv.destroyAllWindows()
-
