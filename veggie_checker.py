76221d4e8a9896c1a19534945ce68b425ddb67df
Dylan Pham <32346117+dylan-pham@users.noreply.github.com>
2019-07-26 09:08:36 -0400

    Update veggie_checker.py

--- veggie_checker.py
+++ veggie_checker.py
@@ -2,8 +2,6 @@ import cv2 as cv
 import numpy as np
 from PIL import Image
 
-cv.startWindowThread()
-
 img = Image.open('images/tush.jpg')
 img = np.array(img)
 ir, ir_n_g, ir_n_r = cv.split(img)
@@ -13,6 +11,10 @@ bottom[bottom == 0] = 0.01
 
 ndvi = ((ir + ir).astype(float) - ir_n_r)/(bottom)
 
-cv.imshow('', ndvi)
-cv.waitKey()
+while True:
+    cv.imshow('', ndvi)
+    
+    if cv.waitKey(0) == ord('q'):
+        break
+        
 cv.destroyAllWindows()
